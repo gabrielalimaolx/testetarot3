c200c2db3124209d2bfbd0b667c26bfe35249aea
gabrielalimaolx <gabrielalimaolx1@gmail.com>
2025-05-13 05:57:31 -0300

    Add files via upload

--- src/components/ReadingResult.tsx
+++ src/components/ReadingResult.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTarot } from '../context/TarotContext';
+import { situationInterpretations, challengeInterpretations, adviceInterpretations } from '../data/interpretations';
 
 const ReadingResult: React.FC = () => {
   const { selectedCards, resetConsultation } = useTarot();
@@ -29,7 +30,11 @@ const ReadingResult: React.FC = () => {
               {selectedCard.title}: {selectedCard.card.name}
             </h3>
             <p className="interpretation-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              {selectedCard.type === 'situation' 
+                ? situationInterpretations[selectedCard.card.name]
+                : selectedCard.type === 'challenge'
+                ? challengeInterpretations[selectedCard.card.name]
+                : adviceInterpretations[selectedCard.card.name]}
             </p>
           </div>
         ))}
